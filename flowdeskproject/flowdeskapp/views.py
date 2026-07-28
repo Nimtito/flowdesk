e6fef53694c4bdae9fa667cc41ebd68e34b6d6d3
nimrod <nimrodtitus12@gmail.com>
2026-07-28 19:06:22 +0300

    import isauthenticated into views.py

--- flowdeskproject/flowdeskapp/views.py
+++ flowdeskproject/flowdeskapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
 from .models import User, Project, Task, Comment, Notification
 from .serializers import (
     UserSerializer,
