f89f9e2d62cbab70a01bb90bf23b9a3eb6755a8b
nimrod <nimrodtitus12@gmail.com>
2026-07-28 10:52:27 +0300

    Add user,project,task,comments and notifications urls

---
+++ flowdeskproject/flowdeskapp/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    UserViewSet,
+    ProjectViewSet,
+    TaskViewSet,
+    CommentViewSet,
+    NotificationViewSet,
+)
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'projects', ProjectViewSet)
+router.register(r'tasks', TaskViewSet)
+router.register(r'comments', CommentViewSet)
+router.register(r'notifications', NotificationViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
