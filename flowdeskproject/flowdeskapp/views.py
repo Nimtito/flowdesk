e2f3e6bc12b6939d782724ac585bf84595d92b5f
nimrod <nimrodtitus12@gmail.com>
2026-07-28 10:54:06 +0300

    Add users,projects,tasks,comments ,notifications views

--- flowdeskproject/flowdeskapp/views.py
+++ flowdeskproject/flowdeskapp/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import User, Project, Task, Comment, Notification
+from .serializers import (
+    UserSerializer,
+    ProjectSerializer,
+    TaskSerializer,
+    CommentSerializer,
+    NotificationSerializer,
+)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class NotificationViewSet(viewsets.ModelViewSet):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
