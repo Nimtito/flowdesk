6b29823221128d082377adc0c0bbfdd8e62d0d6c
nimrod <nimrodtitus12@gmail.com>
2026-07-30 00:12:01 +0300

    import register and profile view and urls

--- flowdeskproject/flowdeskapp/urls.py
+++ flowdeskproject/flowdeskapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import DashboardStatsView
+from .views import RegisterView
+from .views import ProfileView
 from .views import (
     UserViewSet,
     ProjectViewSet,
@@ -18,6 +20,8 @@ router.register(r'comments', CommentViewSet)
 router.register(r'notifications', NotificationViewSet)
 
 urlpatterns = [
-     path("dashboard/", DashboardStatsView.as_view(), name="dashboard"),
+    path("dashboard/", DashboardStatsView.as_view(), name="dashboard"),
     path('', include(router.urls)),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("profile/", ProfileView.as_view(), name="profile"),
 ]
